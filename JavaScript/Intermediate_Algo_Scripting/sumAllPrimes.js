026149686662c716ff23d189c089ee98658be322
Ryan Wang <rwryanwang@gmail.com>
2022-07-11 16:41:02 -0700

    Initial Commit, added Sum All Primes Exercise

---
+++ JavaScript/Intermediate_Algo_Scripting/sumAllPrimes.js
@@ -0,0 +1,44 @@
+/*
+    Ryan Wang
+    FCC - JavaScript Algorithm Scripting
+    Intermediate Algorithm Scripting
+    Sum All Primes
+*/
+
+//My Shitty Solution
+function sumPrimes(num) {
+  let max = [];
+  let prime = [];
+  let sum = 0;
+
+  //Helper function stolen from StackOverflow -- flags true if prime
+  function isPrime (n){
+    if (n < 2) return false;
+    var q = Math.floor(Math.sqrt(n));
+    for (var i = 2; i <= q; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+  //Gets all numbers from 1 to max num into an array
+  for (let i = 1; i <= num; i++) {
+    max.push(i);
+  }
+
+  //Filters primes into new array, then sums them.
+  prime = max.filter(data => isPrime(data));
+  sum = prime.reduce((a,b) => (a + b));
+
+  return console.log(sum);
+
+
+}
+
+sumPrimes(10);
+ 
+
