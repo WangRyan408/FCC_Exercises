3b4e68a2a1dd711786acfe29f472a42b04bac0ff
Wangstaboi <bubmom9@gmail.com>
2022-07-15 15:14:16 -0700

    Added steamroller.js

---
+++ JavaScript/Intermediate_Algo_Scripting/steamroller.js
@@ -0,0 +1,15 @@
+/*
+    Ryan Wang
+    FCC - JavaScript Algorithms and Data Structures
+    Intermediate Algorithm Scripting
+    Steamroller
+    
+   Flatten a nested array. You must account for varying levels of nesting.
+
+*/
+
+function steamrollArray(arr) {
+    return arr;
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]);
