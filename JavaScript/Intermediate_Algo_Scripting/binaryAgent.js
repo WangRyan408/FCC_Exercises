2ead777d51a9852b73fa1ba7ee31814cf437269e
Ryan Wang <rwryanwang@gmail.com>
2022-07-20 18:47:01 -0700

    Finished binaryAgent.js algo

--- JavaScript/Intermediate_Algo_Scripting/binaryAgent.js
+++ JavaScript/Intermediate_Algo_Scripting/binaryAgent.js
@@ -10,8 +10,25 @@
 -
 */
 
+// My Shitty Solution - Completed
+/*
+    binaryArr - takes str and splits each byte of binary into and array
+    textArr - Initialized as empty array, mapped from binary to decimal,
+              and then from decimal to a string using ASCII
+    text - joins textArr using empty spaces
+*/
 function binaryAgent(str) {
-    return str;
+
+    let binaryArr = str.split(" ");
+    let textArr = [];
+    let text = "";
+
+    textArr = binaryArr.map(x => parseInt(x, 2))
+          .map(y => String.fromCharCode(y));
+
+    text = textArr.join(" ");
+    
+    return text;
   }
   
   binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
