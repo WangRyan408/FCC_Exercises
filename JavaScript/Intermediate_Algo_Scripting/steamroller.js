c911da90a24d96f56526c68eb735736c95eaecba
Ryan Wang <rwryanwang@gmail.com>
2022-07-20 18:33:57 -0700

    Added FCC Solution #2, still figuring out algo

--- JavaScript/Intermediate_Algo_Scripting/steamroller.js
+++ JavaScript/Intermediate_Algo_Scripting/steamroller.js
@@ -5,24 +5,35 @@
     Steamroller
     
    Flatten a nested array. You must account for varying levels of nesting.
-
+-
 */
 
+/*
 function steamrollArray(arr) {
+
     let newArr = [];
-
-    
-
     for (let i = 0; i < arr.length; i++) {
         if (Array.isArray(arr[i])) {
-            
+            newArr.push(steamrollArray(arr[i]));
+        } else {
+            newArr.push(arr[i]);
         }
+        
     }
 
+    return console.log(newArr);
 
-    return console.log(arr[1]);
 
   }
-  
- // steamrollArray([1, [2], [3, [[4]]]]);
+  */
+  function steamrollArray(arr) {
+    const flat = [].concat(...arr);
+    return console.log(flat.some(Array.isArray) ? steamrollArray(flat) : flat);
+    //return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+ 
+  }
+  //steamrollArray([1, [2], [3, [[4]]]]);
   steamrollArray([[["a"]], [["b"]]]);
+  
+
+  
