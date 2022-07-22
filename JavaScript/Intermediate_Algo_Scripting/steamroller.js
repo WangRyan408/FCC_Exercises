f0ac60c803b5df34a66cae8f52b6e991531842d4
Wangstaboi <bubmom9@gmail.com>
2022-07-21 22:27:02 -0700

    dunno what i changed

--- JavaScript/Intermediate_Algo_Scripting/steamroller.js
+++ JavaScript/Intermediate_Algo_Scripting/steamroller.js
@@ -12,18 +12,9 @@
 function steamrollArray(arr) {
 
     let newArr = [];
-<<<<<<< HEAD
-
-
-    for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])) {
-            let subArr = steamrollArray(arr[i]);
-            newArr = newArr.concat(subArr);
-=======
     for (let i = 0; i < arr.length; i++) {
         if (Array.isArray(arr[i])) {
             newArr.push(steamrollArray(arr[i]));
->>>>>>> c5644c385d09bb521a94964910b13a191974a4d3
         } else {
             newArr.push(arr[i]);
         }
@@ -32,10 +23,6 @@ function steamrollArray(arr) {
 
     return console.log(newArr);
 
-<<<<<<< HEAD
-    return console.log(newArr);
-=======
->>>>>>> c5644c385d09bb521a94964910b13a191974a4d3
 
   }
   */
