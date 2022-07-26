17688d65e3571ded51ff8c953d920cf234ab4b8e
Ryan Wang <rwryanwang@gmail.com>
2022-07-25 18:12:40 -0700

    Added makeAPerson.js

---
+++ JavaScript/Intermediate_Algo_Scripting/makeAPerson.js
@@ -0,0 +1,19 @@
+/**
+ *  Ryan Wang
+    FCC - JavaScript Algorithms and Data Structures
+    Intermediate Algorithm Scripting
+    Make A Person
+ */
+
+
+const Person = function(firstAndLast) {
+    // Only change code below this line
+    // Complete the method below and implement the others similarly
+    this.getFullName = function() {
+      return "";
+    };
+    return firstAndLast;
+  };
+  
+  const bob = new Person('Bob Ross');
+  bob.getFullName();
