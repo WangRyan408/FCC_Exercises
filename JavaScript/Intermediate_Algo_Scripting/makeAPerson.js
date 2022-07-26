9f1960e6f4567994da61c02854c74cdeaee782bc
Ryan Wang <rwryanwang@gmail.com>
2022-07-26 14:59:19 -0700

    Misc changes to makeAPerson.js

--- JavaScript/Intermediate_Algo_Scripting/makeAPerson.js
+++ JavaScript/Intermediate_Algo_Scripting/makeAPerson.js
@@ -9,11 +9,40 @@
 const Person = function(firstAndLast) {
     // Only change code below this line
     // Complete the method below and implement the others similarly
+    let arr = firstAndLast.split(" ");
+    let firstName = arr[0];
+    let lastName = arr[1];
+    let fullName = firstName + " " + lastName;
+    
+    
+    this.setFirstName = function(first) {
+        this.firstName = first;
+    }
+    
+    this.setLastName = function(last) {
+        this.lastName = last;
+    }
+
+    this.setFullName = function(firstAndLast) {
+        this.fullName = firstAndLast;
+    }
+    
     this.getFullName = function() {
-      return "";
+      return fullName;
     };
+
+    this.getFirstName = function() {
+        return firstName;
+    }
+
+    this.getLastName = function() {
+        return lastName;
+    }
+
+    
     return firstAndLast;
   };
   
   const bob = new Person('Bob Ross');
-  bob.getFullName();
+  
+  console.log(Object.keys(bob).length);
