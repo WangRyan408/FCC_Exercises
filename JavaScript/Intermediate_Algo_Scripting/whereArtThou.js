4fce7286aa5519efd92772321ccc311bc4524854
Wangstaboi <bubmom9@gmail.com>
2022-08-02 16:49:04 -0700

    Updated whereArtThou.js; still trying to figure it out

--- JavaScript/Intermediate_Algo_Scripting/whereArtThou.js
+++ JavaScript/Intermediate_Algo_Scripting/whereArtThou.js
@@ -18,22 +18,48 @@ from the array (the first argument), because it contains the name and its value,
 that was passed on as the second argument.
 */
 
-function whatIsInAName(collection, source) {
+/*function whatIsInAName(collection, source) {
     const arr = [];
-    const keys = Object.keys(source);
-    const values = Object.values(source);
+    const sourceKeys = Object.keys(source);
+    const sourceValues = Object.values(source);
     // Only change code below this line
 
     for (let i = 0; i < collection.length; i++) {
-        for (let j = 0; j < keys.length; j++) {
-            if (collection[i].hasOwnProperty(keys[j]) && Object.values(collection[i]).some(x => x == values[j])) {
+        for (let j = 0; j < sourceKeys.length; j++) {
+            if (collection[i].hasOwnProperty(sourceKeys[j]) || Object.values(collection[i]).some(x => x == sourceValues[j])) {
                 arr.push(collection[i]);
-            }
         }
-  
+        //collection[i].hasOwnProperty(sourceKeys[j])
     // Only change code above this line
 
   }
+}
+
+  
+  //return console.log(Object.keys(source));
+  //return console.log(Object.values(collection[2]));
+  //return console.log(collection[1]);
+  //return console.log(Object.values(collection[1]).some(x => x == Object.values(source)));
+
+  return console.log(sourceKeys);
+  //return arr;
+}
+*/
+
+function whatIsInAName(collection, source) {
+    let arr = [];
+    // [ 'apple', 'bat' ]
+    const sourceKeys = Object.keys(source);
+    
+
+    //arr = collection.filter(obj => sourceKeys.every(key => obj.hasOwnProperty(key) && obj[key] == source[key]));
+    for (let i = 0; i < collection.length; i++) {
+        for (let j = 0; j < sourceKeys.length; j++) {
+            if (collection[i].hasOwnProperty(sourceKeys[j]) && collection[i][sourceKeys[j]] == source[sourceKeys[j]]) {
+                arr.push(collection[i]);
+            }
+        }
+    }
 
 /*
     for (let i = 0; i < collection.length; i++) {
@@ -49,10 +75,9 @@ function whatIsInAName(collection, source) {
   //return console.log(Object.values(collection[2]));
   //return console.log(collection[1]);
   //return console.log(Object.values(collection[1]).some(x => x == Object.values(source)));
-
+  //Object.keys(collection[0]);
   return console.log(arr);
   //return arr;
 }
-
 whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 });
 //whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
