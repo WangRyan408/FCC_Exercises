29d6fd5e2ed90cffa83604e73efaf107029e81c6
Ryan Wang <rwryanwang@gmail.com>
2022-08-05 16:48:17 -0700

    deleted comments in steamroller.js

--- JavaScript/Intermediate_Algo_Scripting/steamroller.js
+++ JavaScript/Intermediate_Algo_Scripting/steamroller.js
@@ -8,34 +8,14 @@
 -
 */
 
-<<<<<<< HEAD
 /*
 
-=======
-
-function steamrollArray(arr) {
-
-    let newArr = [].concat(...arr);
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (newArr.some(Array.isArray)) {
-            steamrollArray(newArr);
-        } else {
-            console.log(newArr);
-        }
-    }
-
-  }
-  
- /*
->>>>>>> e7afd2b8aa23112ac62fa1b76c43ecd9e48fdba2
   function steamrollArray(arr) {
     const flat = [].concat(...arr);
     return console.log(flat.some(Array.isArray) ? steamrollArray(flat) : flat);
     //return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
  
   }
-<<<<<<< HEAD
 */
   function steamrollArray(arr) {
     
@@ -50,9 +30,6 @@ function steamrollArray(arr) {
     //return console.log(flatten);
 
   
-=======
-  */
->>>>>>> e7afd2b8aa23112ac62fa1b76c43ecd9e48fdba2
   //steamrollArray([1, [2], [3, [[4]]]]);
   console.log(steamrollArray([[["a"]], [["b"]]]));
   
