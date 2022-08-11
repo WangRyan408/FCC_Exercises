0b49ea5ee6b92410f3164e2898c116b3e5928747
Wangstaboi <bubmom9@gmail.com>
2022-08-11 14:00:41 -0700

    Added if else to sort return values

--- JavaScript/JS_Algorithms_And_DataStructures_Projects/cashRegister.js
+++ JavaScript/JS_Algorithms_And_DataStructures_Projects/cashRegister.js
@@ -26,29 +26,63 @@ as the value of the change key.
 
 */
 
+
+/**
+ * 
+ * Current plan:
+ *  Calculate amount of money needed through if else statements
+ *  Check to see if calculated amount is greater or lower than amount in "drawer"
+ *  If lower, return open status and cash, if greater, return insufficient and empty arr
+ *  If exact, return closed and return cid
+ */
 function checkCashRegister(price, cash, cid) {
-    let change;
+    
+    const HUNDRED = 100;
+    const TWENTY = 20;
+    const TEN = 10;
+    const FIVE = 5;
+    const ONE  = 1;
+    const QUARTER = 0.25;
+    const DIME = 0.1;
+    const NICKEL = 0.05;
+    const PENNY = 0.01;
 
-    let changeOwed = cash - price;
-    const money = {
-        status : '',
-        change : [],
+    let changeOwed;
+    let totalCash;
 
-        set setStatus(newStatus) {
+    changeOwed = cash - price;
+    totalCash = 0;
 
-
-           this.status = newStatus;
-        },
-        
-        set setChange(newChange) {
-            this.change = newChange;
-        }
-        
+    let monies = {
+        status : "",
+        change : []
     };
 
+    
+    //Getting total cash available in cid into 1 variable - totalCash
+    for (let i = 0; i < cid.length; i++) {
+        //looping through cid and adding money
+        //multiplying by 100 because evil floats
+        totalCash += cid[i][1] * 100;
+    }
 
+    //Dividing to get actual monetary value
+    totalCash /= 100;
 
-    return console.log(cid);
+    //Filtering algo to see what does what
+    if (totalCash > changeOwed) {
+        monies.status = "OPEN";
+        monies.change.push(['one', 0.1])
+    } else if (changeOwed == totalCash) {
+        monies.status = "CLOSED";
+        monies.change.concat(cid);
+    } else {
+        monies.status = "INSUFFICIENT_FUNDS";
+    }
+
+    return console.log(monies);
   }
   
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+  checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
