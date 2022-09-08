455589364c4d4d28dad063382a5f92464a3026d2
Ryan Wang <rwryanwang@gmail.com>
2022-09-07 17:14:18 -0700

    Added PropTypes and passPropsStateless

---
+++ Front End Development Libraries/React/PropTypes.jsx
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @param {*} props 
+ * @returns Items function
+ * This exercise explains propTypes. PropTypes are warning to remind you what props should be.
+ * 
+ */
+
+
+const Items = (props) => {
+    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+  };
+  
+  // Change code below this line
+  Items.propTypes = {
+    quantity: PropTypes.number.isRequired
+  };
+  // Change code above this line
+  
+  Items.defaultProps = {
+    quantity: 0
+  };
+  
+  class ShoppingCart extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return <Items />
+    }
+  };
